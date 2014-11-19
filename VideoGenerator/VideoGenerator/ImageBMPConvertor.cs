e851faab9fc31ea1c52a3ea8a68473c21510f6c9
anfulu36484 <anfulu36484@gmail.com>
2014-11-19 21:29:56 +0400

    VideoGenerator теперь работает с новым форматом изображения. Чтение данных (состояний поля) производится из базы данных.

---
+++ VideoGenerator/VideoGenerator/ImageBMPConvertor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+
+namespace VideoGenerator
+{
+    class ImageBMPConvertor
+    {
+
+        private static void GetWigthAndHeigth(byte[] image, out int wigth, out int heigth)
+        {
+            byte[] widthByteArray = new byte[4];
+            byte[] heigthByteArray = new byte[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                widthByteArray[i] = image[i];
+                heigthByteArray[i] = image[i + 4];
+            }
+
+            wigth = BitConverter.ToInt32(widthByteArray, 0);
+            heigth = BitConverter.ToInt32(heigthByteArray, 0);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="image">Картинка в собственном формате</param>
+        /// <returns></returns>
+        public Bitmap Convert(byte[] image)
+        {
+            int wigth;
+            int heigth;
+            GetWigthAndHeigth(image, out wigth, out heigth);
+
+            Bitmap bitmap = new Bitmap(wigth,heigth);
+
+            int index = 8;
+            for (int i = 0; i < wigth; i++)
+            {
+                for (int j = 0; j < heigth; j++)
+                {
+                    int R = image[index];
+                    index++;
+                    int G = image[index];
+                    index++;
+                    int B = image[index];
+                    index++;
+                    bitmap.SetPixel(i,j,Color.FromArgb(R,G,B));
+                }
+            }
+            bitmap.Save("test.bmp");
+            return bitmap;
+        }
+
+        
+    }
+}
--- VideoGenerator/VideoGenerator/Program.cs
+++ VideoGenerator/VideoGenerator/Program.cs
@@ -36,58 +36,39 @@ namespace VideoGenerator
             }
         }
 
+
+
         static void Main(string[] args)
         {
+            ImageBMPConvertor imageConv = new ImageBMPConvertor();
+            int width = 300;
+            int height = 300;
 
 
+            VideoFileWriter writer = new VideoFileWriter();
+            writer.Open("fractals.avi", width, height, 25, VideoCodec.MPEG4, 100000000);
 
             using (var connection = new SQLiteConnection(string.Format("Data Source={0};Version=3", @"D:\С_2013\Fractals\Data\picBD.db3")))
             using (var command = new SQLiteCommand(connection))
             {
                 connection.Open();
 
-
-                command.CommandText = "SELECT Frame FROM FRAMES WHERE ID = 1";
+                command.CommandText = "SELECT Frame FROM FRAMES";
                 using (var reader = command.ExecuteReader())
                 {
                     while (reader.Read())
                     {
+
                         byte[] buffer = GetBytes(reader);
-                        Console.Write(buffer);
+
+                        writer.WriteVideoFrame(imageConv.Convert(buffer));
                     }
                 }
 
             }
 
 
-
-            /*int width = 600;
-            int height = 600;
-
-            VideoFileWriter writer = new VideoFileWriter();
-            
-
-            string directory = @"D:\С_2013\Fractals\Data\";
-
-
-            DirectoryInfo dir = new DirectoryInfo(directory);
-
-            String [] bmpFiles = dir.GetFiles("*.bmp")
-                .Select(n => Convert.ToInt32(n.Name.Split('.')
-                .ElementAt(0)))
-                .OrderBy(n => n)
-                .Select(n =>string.Format(@"{0}{1}.bmp",dir.FullName,n))
-                .ToArray();
-
-            writer.Open("fractals.avi", width, height, 25, VideoCodec.MPEG4, 100000000);
-
-            for (int i = 0; i < bmpFiles.Length; i++)
-            {
-                Bitmap bitmap = new Bitmap(Bitmap.FromFile(bmpFiles[i]));
-                writer.WriteVideoFrame(bitmap);
-            }
-
-            writer.Close();*/
+            writer.Close();
         }
     }
 }
